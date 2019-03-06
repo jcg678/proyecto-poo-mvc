29d7d201b0a3de2fa1061767bc1e55624f9c4dfe
jcg678 <jcg678@hotmail.es>
2019-03-06 21:15:21 +0100

    maquetada la barra lateral

--- index.php
+++ index.php
@@ -42,16 +42,19 @@
 			<!--Barra Lateral-->
 	        <aside id="lateral">
 	        	<div id="login" class="block_aside">
+	        		<h3>Entrar a la web</h3>
 	        		<form action="#" method="post">
 	        			<label form="email">Email</label>
 	        			<input type="email" name="email" />
 	        			<label for="password">Password</label>
 	        			<input type="password" name="password">
 	        			<input type="submit" value="Enviar">
-	        		</form>	
-	        		<a href="#">Mis pedidos</a>
-	        		<a href="#">Gestionar pedidos</a>
-	        		<a href="#">Gestionar categorias</a>
+	        		</form>
+	        		<ul>	
+	        			<li><a href="#">Mis pedidos</a></li>
+	        			<li><a href="#">Gestionar pedidos</a></li>
+	        			<li><a href="#">Gestionar categorias</a></li>
+	        		</ul>
 	        	</div>
 	        </aside>	
 			<!--Contenido Central-->
