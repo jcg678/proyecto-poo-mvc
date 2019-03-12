3ff240ce33f880b4f9435bc549d93489240b8593
jcg678 <jcg678@hotmail.es>
2019-03-12 19:57:09 +0100

    formulario en usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -5,4 +5,14 @@ class usuarioController{
 		echo "controlador usuarios index";
 	}
 
+	public function registro(){
+		require_once "views/usuario/registro.php";
+	}	
+
+	public function save(){
+		if(isset($_POST)){
+			var_dump($_POST);
+		}
+	}
+
 }
