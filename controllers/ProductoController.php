d53c352b4c45663f680e0db7266358d101df9ace
jcg678 <jcg678@hotmail.es>
2019-03-22 16:55:53 +0100

    listar productos gestion admin

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -1,8 +1,21 @@
 <?php
-
+require_once 'models/producto.php';
 class productoController{
 	public function index(){
 		require_once 'views/producto/destacado.php';
+	}
+
+	public function gestion(){
+		Utils::isAdmin();
+		$producto = new Producto();
+		$productos = $producto->getAll();
+
+		require_once 'views/producto/gestion.php';
+	}
+
+	public function crear(){
+		Utils::isAdmin();
+		require_once 'views/producto/crear.php';
 	}
 
 }
