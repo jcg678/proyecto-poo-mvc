2b2346ca68259e1f8efee61c076a220b5e22d62d
jcg678 <jcg678@hotmail.es>
2019-03-28 20:21:48 +0100

    borrar elemento

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -73,6 +73,20 @@ class productoController{
 	}
 
 	public function eliminar(){
-		var_dump($_GET);
-	}
+ 		Utils::isAdmin();
+ 		if(isset($_GET['id'])){
+ 			$id = $_GET['id'];
+ 			$producto = new Producto();
+ 			$producto->setId($id);
+ 			$delete = $producto->delete();
+ 			if($delete){
+ 				$_SESSION['delete']= 'complete';
+ 			}else{
+ 				$_SESSION['delete']= 'failed';
+ 			}
+ 		}else{
+ 			$_SESSION['delete']= 'complete';
+ 		}
+ 		header('Location:'.base_url.'producto/gestion');
+ 	}
 }
--- views/producto/gestion.php
+++ views/producto/gestion.php
@@ -1,12 +1,22 @@
 <h1>Gestión de productos</h1>
 
 <a href="<?=base_url?>producto/crear" class="button button-small">Crear Producto</a>
+
 <?php if(isset($_SESSION['producto']) && $_SESSION['producto'] == 'complete'): ?>
 	<strong class="alert-green">El producto se ha creado correctamente</strong>
 <?php elseif(isset($_SESSION['producto']) && $_SESSION['producto'] == 'failed'): ?>
 	<strong class="alert-red">No se ha creado correctamente</strong>
 <?php endif; ?>
 <?php Utils::deleteSession('producto'); ?>	
+
+
+<?php if(isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete'): ?>
+	<strong class="alert-green">El producto se ha borrado correctamente</strong>
+<?php elseif(isset($_SESSION['delete']) && $_SESSION['delete'] == 'failed'): ?>
+	<strong class="alert-red">No se ha borrado correctamente</strong>
+<?php endif; ?>
+<?php Utils::deleteSession('delete'); ?>
+
 <table>
 	<tr>
 		<th>ID</th>
@@ -30,8 +40,8 @@
 			<?=$pro->stock;?>
 		</td>
 		<td>
-			<a href="<?=base_url?>producto/editar?id=<?=$pro->id?>" class="button button-gestion">Editar</a>
-			<a href="<?=base_url?>producto/eliminar?id=<?=$pro->id?>" class="button button-gestion button-red">Eliminar</a>			
+			<a href="<?=base_url?>producto/editar&id=<?=$pro->id?>" class="button button-gestion">Editar</a>
+			<a href="<?=base_url?>producto/eliminar&id=<?=$pro->id?>" class="button button-gestion button-red">Eliminar</a>			
 		</td>				
 	</tr>
 <?php endwhile;?>	
