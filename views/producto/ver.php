fd6c5eadc3440a224889daaad5c2cecd3af5eb59
jcg678 <jcg678@hotmail.es>
2019-04-04 20:26:28 +0200

    estilos producto principal

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,15 +1,20 @@
-<h1></h1>
+
 <?php if(isset($pro)): ?>
 <h1><?=$pro->nombre?></h1>
-						<?php if($pro->imagen != null): ?>
-								<img src="<?=base_url?>uploads/images/<?=$pro->imagen?>">
-							<?php else: ?>
-								<img src="<?=base_url?>assets/img/camiseta.png">	
-							<?php endif ?>	
-							
-						<p><?=$pro->precio?> Euros</p>
-						<a href="" class="button">Comprar</a>
-
+	<div id="detail-product">
+		<div class="image">
+			<?php if($pro->imagen != null): ?>
+				<img src="<?=base_url?>uploads/images/<?=$pro->imagen?>">
+			<?php else: ?>
+				<img src="<?=base_url?>assets/img/camiseta.png">	
+			<?php endif ?>	
+		</div>			
+		<div class="data">
+			<p class="description"><?=$pro->descripcion?></p>			
+			<p class="price"> <?=$pro->precio?> Euros</p>
+			<a href="" class="button">Comprar</a>
+		</div>				
+	</div>					
 <?php else: ?>
 	<h1>No existe el producto</h1>
 <?php endif; ?> 
